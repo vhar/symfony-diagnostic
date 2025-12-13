cdb6c30c95940296befee48b3b285efeeaade1b3
vhar <vhar@mail.ru>
2025-12-13 12:55:06 +0300

    chore: add http response code

--- src/Shared/Domain/Exception/ApiExceptionInterface.php
+++ src/Shared/Domain/Exception/ApiExceptionInterface.php
@@ -4,6 +4,8 @@ namespace App\Shared\Domain\Exception;
 
 interface ApiExceptionInterface
 {
+    const HTTP_OK = 200;
+    const HTTP_CREATED = 201;
     const HTTP_BAD_REQUEST = 400;
 
     public function getStatusCode(): int;
